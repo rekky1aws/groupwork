4219e1a0acb6c2df5c6cc76eb21a5b43c06561f8
rekky1aws <killikomfr@gmail.com>
2022-08-04 22:55:57 +0200

    Commented the code

--- wordcloud_poc/wordcloud.js
+++ wordcloud_poc/wordcloud.js
@@ -14,21 +14,36 @@ function getHighestWeight (words) {
 
 async function displayWordCloud ()
 {
+	// On récupère les mots grace a la fonction prévue dans words.js.
 	const words = await getWords();
 	console.log(words);
-	const wordsKeys = await Object.keys(words)
+	// On récupère toutes les clés de words, donc les noms des catégories
+		// C'est ici que les choses ne se passent plus comme je prévoyais...
+		// Question de temporalité, à mon humble avis, words n'a pas eu le temps d'avoir récupéré ses valeurs.
+		// Object.keys retourne un tableau vide.
+	const wordsKeys = Object.keys(words)
 	console.log(wordsKeys);
 
+	// On récupère la catégorie qui apparait le plus souvent.
 	const maxWeight =  getHighestWeight(words);
 
-
+	// Ensuite pour chaque categories, on l'affiche dans le nuage de mot, plus il y a d'articles qui y sont liés, plus elle apparait grande et vive.
+	// A l'inverse, si moins d'artcile sont liés à une catégorie, le nom de celle-ci apparait en plus petit et d'une couleur moins saturée.
 	Object.keys(words).forEach((element) => {
+		// On crée la balise <a></a> qui correspond.
 		let a = document.createElement('a');
+
+		// On y ajoute le nom, on obtient donc <a>Nom</a>
 		a.textContent = element;
+
+		// On ajoute le lien, on a donc <a href="/categories?name=Nom">Nom</a>
 		a.href = "/categories?name=" + element;
+
+		// On change la taille et l'opacité en fonction du nombre d'articles.
 		a.style.fontSize = words[element] + 'em';
 		a.style.opacity = 0.5 + 0.5 * (words[element] / maxWeight);
-		console.log(a);	
+		
+		// On ajoute la balise à celle contenant le nuage de mots
 		container.appendChild(a);
 	});
 
--- wordcloud_poc/words.js
+++ wordcloud_poc/words.js
@@ -1,6 +1,5 @@
 console.log("words.js");
 
-
 /*
 const str = "Lorem ipsum dolor sit amet, consectetur adipiscing elith Donec sed viverra metus Cras felis augue semper at velit at finibus varius mauris Aliquam vulputate, nunc eget hendrerit elementum, libero ligula facilisis orci, ut bibendum diam turpis id lectus Nulla sodales molestie augue in hendrerit enim elementum in Cras ut euismod augue eget faucibus nisl Praesent id ligula eu sem pulvinar viverra Nunc at tempus tellus, laoreet molestie risus Fusce imperdiet faucibus orci, nec molestie felis venenatis nec Duis lectus justo, tincidunt a metus ac, consectetur feugiat odio Nulla sapien diam, pretium ac vulputate non porta porttitor justo Donec tincidunt vestibulum odio in mollis";
 const splittedStr = str.toLowerCase().replace(/[.,]/g, '').split(" ");
@@ -14,27 +13,42 @@ splittedStr.forEach((element) => {
 });
 */
 
-
-
 async function getWords ()
 {
+	// On initialise words.
 	let words = [];
+
 	const apiUrl = "https://127.0.0.1:8000";
 
+	// On va chercher tous les articles.
 	const response = await fetch(apiUrl + '/api/articles/');
 	if(response.ok) {
+		// On récupère le resultat de notre requête au format JSON.
 		const responseJSON = await response.json();
+		// On stocke les différents articles dans la variables correspondante.
 		const articles = responseJSON["hydra:member"]
+
+		// On itère pour chaque article afin de connaitre la fréquence d'une catégorie;
 		articles.forEach(async function (element) {
+			// On va donc chercher le nom de la catégorie qui correspond.
 			const catResponse = await fetch(apiUrl + element.category);
-			const catJSON = await catResponse.json();
-			if (words[catJSON.name]) {
-				words[catJSON.name] ++;
-			} else {
-				words[catJSON.name] = 1;
+			if (catResponse.ok)
+			{
+				// On récupère un objet JSON.
+				const catJSON = await catResponse.json();
+				// Si on a déjà une propriété (clé) de words qui correspond a la categorie, on incrémente sa fréquence (valeur).
+				if (words[catJSON.name]) {
+					words[catJSON.name] ++;
+
+				// Sinon on la crée et on l'initialise à 1.
+				} else {
+					words[catJSON.name] = 1;
+				}
+				// console.log(catJSON.name);
 			}
-			// console.log(catJSON.name);
 		});
 		return words;
 	}
 }
+
+// Une fois que l'on a récupéré la fréquence des catégories, c'est le fichier wordcloud.js qui va se charger de les afficher.
