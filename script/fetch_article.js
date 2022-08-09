50a3aa351cc39ad3ef717de3e7f0cfc817b0a9fe
rekky1aws <killikomfr@gmail.com>
2022-08-09 16:56:27 +0200

    Filtered articles and sorted them by date

--- script/fetch_article.js
+++ script/fetch_article.js
@@ -6,22 +6,29 @@ fetch(myJSON)
   .then((articlesJSON) => {
     // fonction pour trier  un tableau d’objets avec la valeur de date par order croissant
     function sortArticles20() {
-      articlesJSON["hydra:member"].sort(function (x, y) {
-        let firstDate = new Date(x.published_at),
-          SecondDate = new Date(y.published_at);
+      let articleArr = JSON.parse(JSON.stringify(articlesJSON["hydra:member"]));
 
-        if (firstDate < SecondDate) return -1;
-        if (firstDate > SecondDate) return 1;
+      console.log(articleArr);
+
+      articleArr = articleArr.filter( function(x) {
+        if (x.published_at) {
+          return true;
+        }
+      }).sort(function (x, y) {
+
+        if (x.published_at < y.published_at) return -1;
+        if (x.published_at > y.published_at) return 1;
         return 0;
       });
       // boucle pour sortir 20 articles par page
       for (
         let i = 0;
-        i < Math.min(articlesJSON["hydra:member"].length, 20);
+        i < Math.min(articleArr.length, 20);
         i++
       ) {
         divArticles.innerHTML += `<div class="block  md:p-[22.3px] p-6 m-2 max-w-sm bg-[#830002] rounded-lg border border-gray-400 shadow-md hover:bg-stone-300">
-          <p class="mb-2 text-xl font-bold tracking-tight text-white">Titre : ${articlesJSON["hydra:member"][i].title}</p>
+          <p class="mb-2 text-xl font-bold tracking-tight text-white">Titre : ${articleArr[i].title}</p>
+          <p class="mb-2 text-xl font-bold tracking-tight text-white">Date : ${articleArr[i].published_at}</p>
           </div>`;
       }
       console.log(articlesJSON["hydra:member"])
