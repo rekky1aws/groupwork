d3ff027a54c40de55fb4e03f48138808f18ac7f5
rekky1aws <killikomfr@gmail.com>
2022-08-09 17:12:55 +0200

    Commented code

--- script/fetch_article.js
+++ script/fetch_article.js
@@ -6,20 +6,20 @@ fetch(myJSON)
   .then((articlesJSON) => {
     // fonction pour trier  un tableau d’objets avec la valeur de date par order croissant
     function sortArticles20() {
-      let articleArr = JSON.parse(JSON.stringify(articlesJSON["hydra:member"]));
-
-      console.log(articleArr);
+      let articleArr = articlesJSON["hydra:member"];
 
       articleArr = articleArr.filter( function(x) {
+        // On ne récupère que les articles qui ont une date de publication non nulle.
         if (x.published_at) {
           return true;
         }
       }).sort(function (x, y) {
-
+        // On trie le tableau par date de publication chronologique.
         if (x.published_at < y.published_at) return -1;
         if (x.published_at > y.published_at) return 1;
         return 0;
       });
+
       // boucle pour sortir 20 articles par page
       for (
         let i = 0;
@@ -31,7 +31,6 @@ fetch(myJSON)
           <p class="mb-2 text-xl font-bold tracking-tight text-white">Date : ${articleArr[i].published_at}</p>
           </div>`;
       }
-      console.log(articlesJSON["hydra:member"])
     }
     sortArticles20();
   });
