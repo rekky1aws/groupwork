d8042aba761019ceeb9fd035c5a207624a3652f2
rekky1aws <killikomfr@gmail.com>
2022-08-10 23:57:35 +0200

    Authors / Writers list and PATCH

---
+++ script/authorEdit.js
@@ -0,0 +1,96 @@
+	// CONSTANTES GLOBALES
+// Adresse de base de l'API
+const apiUrl = "https://localhost:8000";
+
+// Select pour choisir les Users
+const userSelect = document.querySelector('#user-selector');
+
+// Bouton de validation
+const validateButton = document.querySelector('.validate-button');
+
+	// EVENT LISTENERS
+validateButton.addEventListener('click', sendData);
+
+	// FONCTIONS
+// Fonction pour récupérer sous forme d'objet les paramètres passés dans l'URL.
+function getUrlParams ()
+{
+	// On ne garde que la partie après "?".
+	const urlParts = window.location.href.split("?");
+	let paramObject = {};
+	if (urlParts[1]) {
+		// On sépare chaque paramètre de l'URL.
+		const params = urlParts[1].split("&");
+		// Pour chaque paramètre, on ajoute une couple clé valeur à l'objet que l'on va ensuite retourner.
+		params.forEach(element => {
+			paramObject[element.split('=')[0]] = element.split('=')[1]
+		});
+	}
+	return paramObject;	
+}
+
+async function getUsers ()
+{
+	const response = await fetch(apiUrl + "/api/users/");
+	const responseJSON = await response.json();
+	const users = responseJSON['hydra:member'];
+
+	users.forEach((element) => {
+		let userOption = document.createElement('option');
+		userOption.value = element.id;
+		userOption.textContent = `${element.id} : ${element.email}`;
+		userSelect.append(userOption);
+	});
+
+	
+
+	// On récupère les informations sur le writer dont l'id est passé en paramètre dans l'URL.
+	const authorResponse = await fetch(apiUrl + "/api/writers/" + urlParams.id);
+	if (authorResponse.ok) {
+		const authorJSON = await authorResponse.json();
+
+		// On ne garde que l'id de l'utilisateur correspondant au writer.
+		let userId = authorJSON.user.split('/');
+		userId = parseInt(userId[userId.length - 1]);
+
+		userSelect.selectedIndex = userId - 1;	
+	} else {
+		alert('Auteur inexistant');
+		window.location.replace('/authors/');
+	}
+}
+
+async function sendData ()
+{
+	const writerBase = await fetch(apiUrl + "/api/writers/" + urlParams.id);
+	const writerBaseJSON = await writerBase.json();
+	console.log(writerBaseJSON);
+
+	let requestBody = {
+		"articles": writerBaseJSON.articles,
+		"pages": writerBaseJSON.pages,
+		"user": "/api/users/" + toString(userSelect.selectedIndex + 1)
+	}
+	requestBody.user = "/api/users/" + (userSelect.selectedIndex + 1);
+	console.log(JSON.stringify(requestBody));
+
+	const response = await fetch(apiUrl + "/api/writers/" + urlParams.id, {
+		method: 'PATCH',
+		body: JSON.stringify(requestBody),
+		headers: {
+			'Content-Type': 'application/merge-patch+json',
+		},
+	});
+	const responseJSON = await response.json();
+	console.log(responseJSON);
+}
+
+
+// On récupère les paramètres passés dans l'URL sous forme d'objet.
+const urlParams = getUrlParams();
+if (urlParams.id) {
+	getUsers();
+} else {
+	alert('Vous devez avoir un auteur à éditer.');
+	window.location.replace('/authors/');
+}
---
+++ script/authors.js
@@ -0,0 +1,47 @@
+const apiUrl = "https://localhost:8000"
+const authorsList = document.querySelector('#authors-list');
+
+async function loadAuthors () {
+	const response = await fetch(apiUrl + "/api/writers");
+	const responseJSON = await response.json();
+
+	writers = responseJSON['hydra:member'];
+
+	writers.forEach(async (element) => {
+		console.log(element.id);
+
+		const writerResponse = await fetch(apiUrl + element['@id']);
+		const writerJSON = await writerResponse.json();
+
+		const userResponse = await fetch(apiUrl + writerJSON.user);
+		const userJSON = await userResponse.json();
+
+		let authorDiv = document.createElement('div');
+		authorDiv.className = "author";
+
+		let authorMail = document.createElement('div');
+		authorMail.className = "author-mail";
+		authorMail.textContent = userJSON.email;
+
+		let authorMenu = document.createElement('div');
+		authorMenu.className = "author-menu";
+
+		let deleteButton = document.createElement('button');
+		deleteButton.className = "cliquable author-button author-delete";
+		deleteButton.textContent = "🗑";
+
+		let editButton = document.createElement('button');
+		editButton.className = "cliquable author-button author-edit";
+		editButton.textContent = "📝";
+		editButton.addEventListener('click', function () {
+			window.location.href = "/authors/edit?id=" + element.id;
+		})
+
+		authorMenu.append(editButton, deleteButton)
+		authorDiv.append(authorMail, authorMenu);
+		authorsList.append(authorDiv);
+
+	});
+}
+
+loadAuthors();
