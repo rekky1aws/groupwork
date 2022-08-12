5e269cc6619af9c2cb5aba6a657af37771a8cce6
rekky1aws <killikomfr@gmail.com>
2022-08-12 10:34:48 +0200

    Code review

--- tagsGestion/app.js
+++ tagsGestion/app.js
@@ -1,68 +1,62 @@
-let nameField=document.getElementById('name')
-let tagList = document.getElementById('tags-select')
-let categoryName = []
-let infoMessage=document.getElementById('info')
-
-
+let nameField = document.getElementById('name');
+let tagList = document.getElementById('tags-select');
+let categoryName = [];
+let infoMessage = document.getElementById('info');
 
 async function getTags() {
-    nameField.value=''
-    const JsonResponse = await (fetch("https://127.0.0.1:8000/api/tags"))
-    const response = await JsonResponse.json()
+    nameField.value = '';
+    const jsonResponse = await fetch("https://127.0.0.1:8000/api/tags");
+    const response = await jsonResponse.json();
     response['hydra:member'].forEach(response => {
       
-      let selectCategory = document.createElement('option')
-      selectCategory.innerHTML = `${response.id}:${response.name}`
-      selectCategory.name=response.name
-      selectCategory.value = response.id
-      tagList.appendChild(selectCategory)
-      categoryName.push(response.name)
-    })
+      let selectCategory = document.createElement('option');
+      selectCategory.innerHTML = `${response.id} : ${response.name}`;
+      selectCategory.name = response.name;
+      selectCategory.value = response.id;
+      tagList.appendChild(selectCategory);
+      categoryName.push(response.name);
+    });
   }
 
-getTags()
-console.log(categoryName)
+getTags();
 
- function categoryNamefuction(){
-     console.log(tagList.value)
-   if(tagList.value!='nouveau tag'){
-    nameField.value=categoryName[parseInt(tagList.value)-1]
- }else{
-     nameField.value=''
- }
+function categoryNamefuction(){
+    if (tagList.value != 'nouveau tag') {
+        nameField.value = categoryName[parseInt(tagList.value) - 1];
+    } else {
+        nameField.value = '';
+    }
 }
-       
-  
 
-tagList.addEventListener('change',categoryNamefuction)
+tagList.addEventListener('change',categoryNamefuction);
 
 async function updateTag(){
-    let APIURL=null
-    let HTTPMethod=null
-    if(tagList.value==='nouveau tag'){
-        APIURL='https://127.0.0.1:8000/api/tags'
-        HTTPMethod='POST'
-    }else{
-        APIURL='https://127.0.0.1:8000/api/tags/'+ (tagList.value)
-        HTTPMethod='PUT'
+    let APIURL = null;
+    let HTTPMethod = null;
+    if (tagList.value === 'nouveau tag') {
+        APIURL = 'https://127.0.0.1:8000/api/tags';
+        HTTPMethod = 'POST';
+    } else {
+        APIURL = 'https://127.0.0.1:8000/api/tags/' + (tagList.value);
+        HTTPMethod = 'PUT';
     }
-    const JsonResponse = await fetch(APIURL,{
+    const JsonResponse = await fetch(APIURL, {
         method: HTTPMethod,
-        headers:{
+        headers: {
             'Content-Type':'application/json'
         },
         body: JSON.stringify({
             name: nameField.value
-        })});
-    if(JsonResponse.status==200 ){
-        infoMessage.innerHTML= "tag modifié" 
-    }else if(JsonResponse.status==201 ){
-        infoMessage.innerHTML= "tag créé" 
-    }
-    else{
-        infoMessage.innerHTML= 'une erreur est survenue'
+        })
+    });
+
+    if (JsonResponse.status == 200) {
+        infoMessage.innerHTML = "tag modifié";
+    } else if (JsonResponse.status == 201) {
+        infoMessage.innerHTML = "Tag créé";
+    } else {
+        infoMessage.innerHTML = 'Une erreur est survenue';
     }
 }
 
-document.getElementById('update').addEventListener('click',updateTag)
-
+document.getElementById('update').addEventListener('click',updateTag);
